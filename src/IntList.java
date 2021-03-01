ce8959c18d5002cef0e01dfc2b6ad3e0bbce87e8
huanhuan <huangganghuan1@gmail.com>
2021-02-28 22:53:58 -0800

    int list implement ,size recursive and iterative

---
+++ src/IntList.java
@@ -0,0 +1,41 @@
+public class IntList {
+    public int first;
+    public IntList rest;
+
+    public IntList(int first,IntList rest){
+        this.first = first;
+        this.rest = rest;
+    }
+
+    /**
+    return size of the list using recursion
+     */
+    public int size(){
+        if(rest == null){
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
+
+    /**
+     return size of the list no recursion
+     */
+    public int iterativeSize(){
+        IntList p = this;
+        int count = 0;
+
+        while(p!= null){
+            count ++;
+            p = p.rest;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        IntList L = new IntList(15,null);
+        L = new IntList(10,null);
+        L = new IntList(5,null);
+    }
+
+
+}
